9cbb310baff56ad18b2ad7b1b8fe4709b9134337
Viet Hung Nguyen <vhn@sfu.ca>
2023-08-05 02:52:09 -0700

    First commit

---
+++ api/index.py
@@ -0,0 +1,48 @@
+from os.path import join
+
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+def preload():
+    print('Preloading...')
+    with open(join('data', 'pi-million.txt'), 'r') as f:
+        return f.read()
+
+data = preload()  
+length = len(data)
+RANGE = 5
+
+@app.route('/find/<digits>', methods=['GET'])
+def hello(digits: str):
+    # Check all characters is digit
+    for c in digits:
+        if not c.isdigit():
+            return Response(f'Input must be a string of numbers', 400)
+    # Find index of digits
+    index = data.find(digits)
+    if (index < 0):
+        return {
+            'length': length,
+            'before': '',
+            'after': f'3.{data[:10]}'
+        }
+    
+    # Get before and after
+    beforeStart = max(index - RANGE, 0)
+    before = data[beforeStart: index]
+    if beforeStart > 0:
+        before = f'...{before}'
+    else:
+        before = f'3.{before}'
+    afterStart = min(index + len(digits), length)
+    afterEnd = min(afterStart + RANGE, length)
+    after = f'{data[afterStart: afterEnd]}...'
+    
+    return {
+        'length': length,
+        'index': index,
+        'before': before,
+        'after': after
+    }
+
